c0ce91bbeedf7fc35fc6487ab66dfa84b5440734
Tobiloba <35341202+towbee98@users.noreply.github.com>
2022-06-02 20:34:42 +0000

    Add the play and exit option

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 let username;//Holds the username of the player
+let range=2;
+let quit=false;
 let stage=1;//Holds the current stage of the player
 let wrongGuess=false ;//tracks whether the player guessed right
-let range=2;
 let points=0;
 
-//this function generates a random between 1 and the a range passed in as a parameter
+//this function generates a random between 1 and  a range passed in as a parameter
 const GenerateNumber= (range)=>{
   return Math.floor(Math.random()*range+1)
 }
 
-//Stores the username of the player
-username= prompt("Enter a username");
 
-
-while(!wrongGuess){
+const RunGuessGame=()=>{
+  while(!wrongGuess){
   const generatedNumber= GenerateNumber(range);//Generates a number
   //Ask the user to guess the number and store it
   const userGuess= parseInt(prompt(`Guess a number between 1 and ${range}`))
@@ -35,5 +34,29 @@ while(!wrongGuess){
     console.log(`Invalid input`)
   }
   
-  
 }
+//Resets the variables back to the default values
+stage=1,range=2,wrongGuess=!wrongGuess,points=0;
+}
+
+
+const playGame= ()=>{
+  while(!quit){
+   const decision=  parseInt(prompt("Ready to play ?1.Yes 2.Exit"));
+    if(decision===1){
+      RunGuessGame();
+    }
+    else if(decision== 2){
+      quit=!quit;
+    }else{
+      console.log("Enter a valid digit")
+    }
+  }
+}
+
+
+//Stores the username of the player
+username= prompt("Enter a username");
+
+//This function beginis starts the game
+playGame();
