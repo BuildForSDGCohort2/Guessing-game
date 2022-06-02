404a5516e9d0b122f49e3414e717bc626f0b53f5
Tobiloba <35341202+towbee98@users.noreply.github.com>
2022-06-02 17:00:36 +0000

    Add a comments to the code for readablitiy

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-let username;//Holds the username 
-let stage=1;
-let wrongGuess=false ;
+let username;//Holds the username of the player
+let stage=1;//Holds the current stage of the player
+let wrongGuess=false ;//tracks whether the player guessed right
 let range=2;
 let points=0;
+
+//this function generates a random between 1 and the a range passed in as a parameter
 const GenerateNumber= (range)=>{
   return Math.floor(Math.random()*range+1)
 }
-  username= prompt("Enter a username");
+
+//Stores the username of the player
+username= prompt("Enter a username");
+
+
 while(!wrongGuess){
-  const generatedNumber= GenerateNumber(range);
+  const generatedNumber= GenerateNumber(range);//Generates a number
+  //Ask the user to guess the number and store it
   const userGuess= parseInt(prompt(`Guess a number between 1 and ${range}`))
+
+  //This checks if the user entered a valid number
   if(userGuess>=1 && userGuess <=range && !isNaN(userGuess)){
-          if(generatedNumber===userGuess){
-          range+=1;
-          stage+=1;
-          points+=1;
-          console.log(`Congrats ${username},you won .You are unto Stage ${stage} .You now have ${points} points...`);
-          
+          if(generatedNumber===userGuess){//This checks if the user guessed right
+          range+=1;//increase the range
+          stage+=1;//move to the next stage
+          points+=1;//gets some points
+          console.log(`Congrats ${username},you won .You are unto Stage ${stage} .You now have ${points} points...`); 
         }
-        else{
+        else{//this works if the user guessed wrongly
           console.log(`Wrong ${username}, The correct number is ${generatedNumber}. Total points : ${points} points`);
           console.log("GAME OVER !!!")
-          wrongGuess=!wrongGuess;
+          wrongGuess=!wrongGuess;//this stops the loop
         }
-  }else{
+  }else{//this notify the user in case wrong input 
     console.log(`Invalid input`)
   }
   
